8b176d6745e59d21c88050ebb1b838bf7c579cd7
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2024-04-18 08:45:16 +0900

    0.24.7

--- keyboards/tarohayashi/killerwhale/duo/halconf.h
+++ keyboards/tarohayashi/killerwhale/duo/halconf.h
@@ -4,3 +4,4 @@
 #define HAL_USE_I2C TRUE
 #define HAL_USE_SPI TRUE
 #include_next <halconf.h>
+
--- keyboards/tarohayashi/killerwhale/solo/keymaps/via/keymap.c
+++ keyboards/tarohayashi/killerwhale/solo/keymaps/via/keymap.c
@@ -2,8 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
-#include "add_keycodes.h"
-#include "add_joystick.h"
+#include "lib/add_keycodes.h"
 
 // レイヤー名
 enum layer_number {
@@ -25,11 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  LSFT_T(KC_Z), KC_X,                 KC_C, KC_V,                    KC_B,
                        MOD_SCRL,
         // 側面スイッチ
-        KC_LNG2, KC_SPACE,                                  
+        KC_LNG2, KC_SPACE,                             
         // 十字キーorジョイスティック                // ジョイスティックスイッチ
-        KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,         KC_ENT,                 
-        // 追加スイッチ                             // トグルスイッチ
-        KC_MS_BTN2, KC_MS_BTN1,                    MO(RIGHT_BASE)
+        KC_W, KC_S, KC_A, KC_D,                  L_CHMOD,                 
+        KC_MS_BTN2, KC_MS_BTN1,                  MO(RIGHT_BASE)
     ),    
     [RIGHT_BASE] = LAYOUT(
         KC_BSPC,    KC_0,       KC_9,            KC_8,            LT(BALL_SETTINGS, KC_7), LT(LIGHT_SETTINGS, KC_6),
@@ -38,24 +36,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_SLSH,    KC_DOT,          KC_COMM,         KC_M,                 KC_N,
                                 MOD_SCRL,
         KC_SPACE, KC_LNG1,
-        KC_LEFT, KC_RIGHT, KC_DOWN, KC_UP,       KC_ENT,
+        KC_LEFT, KC_RIGHT, KC_DOWN, KC_UP,        L_CHMOD,
         KC_MS_BTN2, KC_MS_BTN1,                  _______      
     ),
     [MOUSE] = LAYOUT(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,
-        _______, XXXXXXX, _______, KC_MS_BTN2, KC_MS_BTN1, MOD_SCRL,
-                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        _______, _______, _______, _______, _______,    _______,
+        _______, _______, _______, _______, _______,    _______,
+        _______, _______, _______, KC_MS_BTN2, KC_MS_BTN1, MOD_SCRL,
+                 QK_USER_4, _______, _______, _______, _______,
                           MOD_SCRL,
-        XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
-        XXXXXXX, XXXXXXX,                            XXXXXXX
+        _______, _______,
+        _______, _______, _______, _______,          _______,
+        _______, _______,                            _______
     ),
     [BALL_SETTINGS] = LAYOUT(
-        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, _______,  L_CHMOD,
-        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, CPI_I,    XXXXXXX,
-        AUTO_MOUSE, XXXXXXX, XXXXXXX, L_ANG_D,   L_INV, L_ANG_I,
-                    TG(RIGHT_BASE), XXXXXXX, XXXXXXX, CPI_D,    XXXXXXX,
+        XXXXXXX,    XXXXXXX, XXXXXXX, QK_USER_14, _______,  L_CHMOD,
+        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, L_SPD_I,    XXXXXXX,
+        AUTO_MOUSE, XXXXXXX, XXXXXXX, L_ANG_D,  L_INV, L_ANG_I,
+                    TG(RIGHT_BASE), XXXXXXX, XXXXXXX, L_SPD_D,    XXXXXXX,
                           INV_SCRL,
         XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
@@ -64,9 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LIGHT_SETTINGS] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, RGB_MOD, _______,
         XXXXXXX, RGB_SPI, RGB_VAI, RGB_SAI, RGB_HUI, RGB_TOG,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        OLED_MOD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                  RGB_SPD, RGB_VAD, RGB_SAD, RGB_HUD, XXXXXXX,
-                          XXXXXXX,
+                          QK_USER_15,
         RGB_MOD, RGB_MOD,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
         XXXXXXX, XXXXXXX,                            XXXXXXX
@@ -75,37 +73,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [LEFT_BASE] =   { 
-        ENCODER_CCW_CW(KC_ESC, KC_TAB), 
+        ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
         ENCODER_CCW_CW(REDO, UNDO), 
         ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
-        ENCODER_CCW_CW(KC_WH_U, KC_WH_D)
+        ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
+        ENCODER_CCW_CW(XXXXXXX, XXXXXXX)
     },
     [RIGHT_BASE] =   { 
         ENCODER_CCW_CW(KC_DEL, KC_BSPC), 
         ENCODER_CCW_CW(KC_UP, KC_DOWN), 
         ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
-        ENCODER_CCW_CW(KC_WH_U, KC_WH_D)
+        ENCODER_CCW_CW(KC_WH_U, KC_WH_D),
+        ENCODER_CCW_CW(XXXXXXX, XXXXXXX)
     },
     [LIGHT_SETTINGS] = { 
         ENCODER_CCW_CW(RGB_SPI, RGB_SPD), 
         ENCODER_CCW_CW(RGB_VAI, RGB_VAD), 
         ENCODER_CCW_CW(RGB_SAI, RGB_SAD), 
-        ENCODER_CCW_CW(RGB_HUI, RGB_HUD)
+        ENCODER_CCW_CW(RGB_HUI, RGB_HUD),
+        ENCODER_CCW_CW(XXXXXXX, XXXXXXX)
     }
 };
 
-// 初期化
-void matrix_init_user(void) {
-    matrix_init_addedjoystick();
-}
 
-// キースキャン
-bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    process_record_addedkeycodes(keycode, record);
-    return true;
-}
 
-// マトリックススキャン
-void matrix_scan_user(void) {
-    matrix_scan_addedjoystick();
-}
+
