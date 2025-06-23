f3b89a096d0f09e6a8ecd5c64e4b6f842fdc0161
Jorres Tarasov <jorres.tarasov@gmail.com>
2025-06-23 11:14:18 +0200

    blade

--- keyboards/tarohayashi/killerwhale/duo/keymaps/jorres/keymap.c
+++ keyboards/tarohayashi/killerwhale/duo/keymaps/jorres/keymap.c
@@ -220,19 +220,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [BLADE] = LAYOUT(
         // 左手
-         KC_TAB,  KC_Q,      KC_1,    KC_2, KC_3, TO(BASE),
-        KC_LCTL, KC_A,      KC_W,       KC_E, KC_R, KC_T,
-        KC_LSFT, KC_Z,      KC_S,       KC_D, KC_F, KC_G,
+        KC_TAB,  KC_Q,      KC_1,       KC_2, KC_3, KC_4,
+        KC_LCTL, KC_A,      KC_W,       KC_E, KC_R, KC_5,
+        KC_LSFT, KC_Z,      KC_S,       KC_D, KC_F, KC_6,
         KC_X,   KC_X, KC_C, KC_V, KC_B,    // <--- first button does not work on hardware level, probably soldering error or TRRS short circuiting
-        MO(NAVIGATION),
+        KC_T,
         KC_SPC, KC_ENT,
-        _______, _______, KC_LALT, KC_ESC,  _______, // first four are d-pad on the left  half
-        // KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,  _______,
+        // _______, _______, KC_LALT, KC_ESC,  _______, // first four are d-pad on the left  half
+        KC_UP, KC_DOWN, KC_LEFT, KC_RIGHT,  _______,
         _______, _______,                    _______,
 
 
         // 右手
-        PRTSCR,  HARPOON_1, HARPOON_2,  KC_0,    KC_F11,  KC_DELETE,
+        TO(BASE),  HARPOON_1, HARPOON_2,  KC_0,    KC_F11,  KC_DELETE,
         KC_Y,    KC_U,      I_ESC,      KC_O,    KC_P,    LBRC_RCTL,
         KC_H,    KC_J,      KC_K,       KC_L,    KC_SCLN, QUOT_RSFT,
         KC_N,    KC_M,      KC_COMM,    KC_DOT,  KC_SLSH,
